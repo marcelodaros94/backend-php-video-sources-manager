c51a099d71dbf5725b08d8f71d478248c6860e65
Marcelo <marcelodaros94@gmail.com>
2023-05-18 23:06:38 -0500

    adapting to more links

--- database/migrations/2023_05_19_034546_create_links_table.php
+++ database/migrations/2023_05_19_034546_create_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVideosTable extends Migration
+class CreateLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateVideosTable extends Migration
      */
     public function up()
     {
-        Schema::create('videos', function (Blueprint $table) {
+        Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->text('title');
-            $table->text('description');
-            $table->tinyText('type');
-            $table->text('image');
-            $table->tinyInteger('rating');
+            $table->text('url');
+            //$table->foreign('source_id')
+                //->references('id')->on('sources');
+            $table->foreign('video_id')
+                ->references('id')->on('videos');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateVideosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('videos');
+        Schema::dropIfExists('links');
     }
 }
